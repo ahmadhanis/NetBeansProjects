fc2eaf012e41dbed7fb61e54d0eb4da953a0949b
ahmadhanis <slumberjer@gmail.com>
2019-11-21 14:21:54 +0800

    21/11

--- BasicGui/src/basicgui/BasicGui.java
+++ BasicGui/src/basicgui/BasicGui.java
@@ -5,22 +5,22 @@
  */
 package basicgui;
 
-
-import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author user
  */
-public class BasicGui  {
-    
+public class BasicGui {
 
     /**
      * @param args the command line arguments
@@ -28,63 +28,71 @@ public class BasicGui  {
     public static void main(String[] args) {
         // TODO code application logic here
         ArrayList<Student> studentlist = new ArrayList<Student>();
-                
+        
         JFrame frame = new JFrame("Basic Interface");
         frame.setSize(300, 400);
-        
+
         JLabel jlabel = new JLabel("Name");
         jlabel.setBounds(10, 10, 60, 20);
         frame.add(jlabel);
-        
+
         JLabel jlabela = new JLabel("Matric No");
         jlabela.setBounds(10, 30, 60, 20);
         frame.add(jlabela);
-       
+
         JLabel jlabelb = new JLabel("Phone");
         jlabelb.setBounds(10, 50, 60, 20);
         frame.add(jlabelb);
-        
+
         JTextField jtfname = new JTextField();
-        jtfname.setBounds(80,10,150,20);
+        jtfname.setBounds(80, 10, 150, 20);
         frame.add(jtfname);
-        
+
         JTextField jtfmatric = new JTextField();
-        jtfmatric.setBounds(80,30,150,20);
+        jtfmatric.setBounds(80, 30, 150, 20);
         frame.add(jtfmatric);
-        
+
         JTextField jtfphone = new JTextField();
-        jtfphone.setBounds(80,50,150,20);
+        jtfphone.setBounds(80, 50, 150, 20);
         frame.add(jtfphone);
-        
-        TextArea jtextarea = new TextArea();
-        jtextarea.setBounds(20,110,220,200);
-        frame.add(jtextarea);
-        
-        
+
         JButton jbutton = new JButton("INSERT");
         jbutton.setBounds(80, 80, 100, 20);
         frame.add(jbutton);
+        
+        //table creation
+        String header[] = new String[]{"Matric", "Name", "Phone"};
+        DefaultTableModel dtm = new DefaultTableModel(header, 1);
+        JTable jtable = new JTable();
+        jtable.setBounds(20, 110, 220, 200);
+        frame.add(jtable);
+        jtable.setModel(dtm);
+        JScrollPane scrollPane = new JScrollPane(jtable);
+        scrollPane.setBounds(20, 110, 220, 200);
+        frame.add(scrollPane);
+        jtable.getColumnModel().getColumn(0).setPreferredWidth(70);
+        jtable.getColumnModel().getColumn(1).setPreferredWidth(150);
+        jtable.getColumnModel().getColumn(2).setPreferredWidth(100);
 
-        jbutton.addActionListener(new ActionListener(){
+        jbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                String name =jtfname.getText();
+                String name = jtfname.getText();
                 String matric = jtfmatric.getText();
                 String phone = jtfphone.getText();
-                studentlist.add(new Student(name,matric,phone));
-                jtextarea.setText("Matric\tName\t\tPhone\n");
-                for (int i =0; i<studentlist.size();i++){
-                    jtextarea.append(studentlist.get(i).getMatric()+"\t"+studentlist.get(i).getName()+
-                            "\t\t"+studentlist.get(i).getPhone()+"\n");
+                studentlist.add(new Student(name, matric, phone));//create object list array
+                dtm.setRowCount(0);//reset table
+                for (int i = 0; i < studentlist.size(); i++) {//populate table using object list
+                    Object[] objs = {studentlist.get(i).getMatric(), studentlist.get(i).getName(), studentlist.get(i).getPhone()};
+                    dtm.addRow(objs);
                 }
             }
         });
-        
+
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
     }
 
-    
 }
